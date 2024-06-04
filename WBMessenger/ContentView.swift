56cd9c5c1d22369549d8cc92d0836e287a3bde29
WayTwoOne <vladimirzakharovj@yandex.ru>
2024-06-04 12:28:39 -0700

    Initial Commit

---
+++ WBMessenger/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WBMessenger
+//
+//  Created by Vladimir on 04.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WBMessenger/WBMessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WBMessengerApp.swift
+//  WBMessenger
+//
+//  Created by Vladimir on 04.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct WBMessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
